b15d7a19a16fba8d8eebd510c4711fc9f77b8421
Anthony Duque <duquea95@gmail.com>
2019-04-03 18:24:38 -0400

    update

--- data-vis/sketch.js
+++ data-vis/sketch.js
@@ -3,7 +3,10 @@
 	Mmp310 Week 8
  */
 
+// Array Variable
  var lakers;
+
+// Preloads CSV File
  function preload(){
 	 lakers = loadTable('lakers.csv', 'csv', 'header');
  }
@@ -14,9 +17,11 @@ function setup() {
 	noStroke();
 }
 
+// Draw Data Visualization
 function draw(){
 	background(51);
 
+	// Get Points from CSV File into PTS array
 	for(let i = 0; i < lakers.getRowCount(); i++){
 		let pts = lakers.getNum(i, 'PTS');
 		let h = map(pts,0,1505, 0, height);
@@ -24,6 +29,7 @@ function draw(){
 		let x = i * (w+3);
 
 		fill('white');
+		// Display Data (Player + Points) when graph is hovered over
 		if (mouseX > x && mouseX < x + w){
 			let player = lakers.getString(i, 'Player').split('\\')[0];
 			text(player, 600, 200);
