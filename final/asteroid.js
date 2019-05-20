0850d5d600a81afec8c9ebd9ef94c17b1dc19a45
Anthony Duque <duquea95@gmail.com>
2019-05-19 20:33:55 -0400

    update

---
+++ final/asteroid.js
@@ -0,0 +1,31 @@
+class Asteroid extends Entity {
+	constructor() {
+		super(random(width), -100);
+		this.speed.x = random(-2, 2);
+		this.speed.y = random(4);
+		this.size = random(20, 100);
+		this.color = color(random(255), random(255), random(255));
+	}
+
+	display() {
+		fill(this.color);
+		noStroke();
+		image(asteroidImg, this.x, this.y, this.size, this.size);
+	}
+
+	update() {
+		super.update();
+
+		// remove asteroids below the canvas
+		if (this.y > height + this.size) {
+			this.remove(asteroids);
+		}
+
+		// bounce asteroids off sides
+		if (this.x <= 0 || this.x >= width) {
+			this.speed.x *= -1;
+		}
+
+
+	}
+}
